a81939852d5c4dd1efd66261a9e9cf68913e4c7a
Kyle Mitchell <kyleevanmitchell@gmail.com>
2013-01-27 19:41:45 -0600

    metadata data types

--- spec/lib/precedent/parser_spec.rb
+++ spec/lib/precedent/parser_spec.rb
@@ -47,23 +47,52 @@ describe Precedent do
       }]
     end
 
-    it "recognizes metadata" do
-      Precedent.parse(<<-eos
+    context 'metadata' do
+      it "recognizes metadata" do
+        Precedent.parse(<<-eos
 #{word.capitalize}: #{first}
 #{another_word.capitalize}: #{second}
 
-  #{third}
-      eos
-      ).should == [
-        {
-          :type => :meta,
-          :content => {
-            word.capitalize.to_sym => first,
-            another_word.capitalize.to_sym => second
-          }
-        },
-        {:type => :indented, :content => third }
-      ]
+#{third}
+        eos
+        ).should == [
+          {
+            :type => :meta,
+            :content => {
+              word.capitalize.to_sym => first,
+              another_word.capitalize.to_sym => second
+            }
+          },
+          {:type => :flush, :content => third }
+        ]
+      end
+
+      it "casts metadata" do
+        num = (1 + rand(1000)).to_s
+        date = (Date.today - (1 + rand(1000)))
+        truth = ['yes','true','True','Yes'].sample
+        falsity = ['no', 'false', 'False', 'No'].sample
+        Precedent.parse(<<-eos
+#{word.capitalize}: #{num}
+#{another_word.capitalize}: #{date.strftime('%Y-%m-%d')}
+#{word.capitalize + another_word}: #{truth}
+#{another_word.capitalize + word}: #{falsity}
+
+#{third}
+        eos
+        ).should == [
+            {
+              :type => :meta,
+              :content => {
+                word.capitalize.to_sym => num.to_i,
+                another_word.capitalize.to_sym => date,
+                (word.capitalize + another_word).to_sym => true,
+                (another_word.capitalize + word).to_sym => false
+              }
+            },
+            {:type => :flush, :content => third }
+          ]
+      end
     end
   end
 end
