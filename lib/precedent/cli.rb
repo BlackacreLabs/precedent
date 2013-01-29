ac943443e9d9e57444b67faeffe94d46587dd267
Kyle Mitchell <kyleevanmitchell@gmail.com>
2013-01-29 17:49:49 -0600

    json output

--- lib/precedent/cli.rb
+++ lib/precedent/cli.rb
@@ -3,10 +3,11 @@ require 'thor'
 module Precedent
   # Command Line Interface
   class CLI < Thor
-    option :file,
+    class_option :file,
       :aliases => '-f',
       :type => :string,
       :desc => 'File to translate'
+
     desc "html", "Translate Precedent markup into HTML5"
     def html()
       if options[:file]
@@ -16,6 +17,18 @@ module Precedent
       end
       require_relative 'html'
       STDOUT.write Precedent.to_html(input)
+    end
+
+    desc "json", "Translate Precedent markup into JSON"
+    def json()
+      if options[:file]
+        input = open(options[:file], 'r').read
+      else
+        input = STDIN.read
+      end
+      require 'json'
+      require_relative 'load'
+      STDOUT.write(JSON::pretty_generate(Precedent.load(input)))
     end
   end
 end
