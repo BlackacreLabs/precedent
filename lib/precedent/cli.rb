413b927e403788fc85e564dc3f0731296f7df4af
Kyle Mitchell <kyleevanmitchell@gmail.com>
2013-01-29 21:42:22 -0600

    yaml output

--- lib/precedent/cli.rb
+++ lib/precedent/cli.rb
@@ -3,49 +3,48 @@ require 'thor'
 module Precedent
   # Command Line Interface
   class CLI < Thor
-    class_option :file,
-      :aliases => '-f',
-      :type => :string,
-      :desc => 'File to translate'
-
-    desc "html", "Translate Precedent markup into HTML5"
-    def html()
-      if options[:file]
-        input = open(options[:file], 'r').read
-      else
-        input = STDIN.read
-      end
+    desc "html [FILE]", "Translate Precedent markup into HTML5"
+    def html(file=STDIN)
       require_relative 'html'
       STDOUT.write Precedent.to_html(input)
     end
 
-    desc "json", "Translate Precedent markup into JSON"
-    def json()
-      if options[:file]
-        input = open(options[:file], 'r').read
-      else
-        input = STDIN.read
-      end
-      require 'json'
+    option :indent,
+      :aliases => '-i',
+      :type => :boolean,
+      :default => false,
+      :desc => "Output indented JSON"
+    desc "json [FILE]", "Translate Precedent markup into JSON"
+    def json(file=STDIN)
+      input = file.is_a?(String) ? File.read(file) : file.read
       require_relative 'load'
-      STDOUT.write(JSON::pretty_generate(Precedent.load(input)))
+      require 'json'
+      STDOUT.write(
+        if options[:indent]
+          JSON::pretty_generate(Precedent.load(input))
+        else
+          JSON::generate(Precedent.load(input))
+        end
+      )
     end
 
-    desc "syntax", "Check markup syntax"
-    def syntax()
-      if options[:file]
-        input = open(options[:file], 'r').read
-      else
-        input = STDIN.read
-      end
+    desc "yaml [FILE]", "Translate Precedent markup into YAML"
+    def yaml(file=STDIN)
+      input = file.is_a?(String) ? File.read(file) : file.read
+      require_relative 'load'
+      require 'yaml'
+      STDOUT.write(Precedent.load(input).to_yaml)
+    end
+
+    desc "syntax [FILE]", "Check markup syntax"
+    def syntax(file=STDIN)
+      input = file.is_a?(String) ? File.read(file) : file.read
       require_relative 'load'
       begin
         Precedent.load(input)
         puts "Syntax OK"
       rescue Exception => e
-        if options[:file]
-          STDERR.write options[:file] + ': '
-        end
+        STDERR.write options[:file] + ': ' if options[:file]
         STDERR.puts e.to_s
         exit 1
       end
